6879f35e7cac79ab84954ea5c2594d26aac92892
Nathan Newman <lordnate.moose@gmail.com>
2022-09-26 17:30:39 -0500

    survey error

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
+app = Flask(__name__)
+
+@app.route('/')
+def load_home():
+    return render_template("home.html")
+
+@app.route('/satisfaction_survey')
+def load_survey():
+    survey = surveys.satisfaction_survey
+    questions = survey.questions
+    import pdb
+    pdb.set_trace()
+    return render_template("survey.html", questions=questions)
