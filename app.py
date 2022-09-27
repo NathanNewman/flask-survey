c1f80dc189f1ce2afda997ebe6a53b4b84d70aa5
Nathan Newman <lordnate.moose@gmail.com>
2022-09-27 17:03:14 -0500

    updated code

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
 app = Flask(__name__)
-
-response = []
-if session["satisfaction"]:
-    responses = session["satisfaction"]
-
+app.config["SECRET_KEY"] = "SHHHHHHHHHHH SEEKRIT"
 
 @app.route('/')
 def load_home():
+    session['satisfaction'] = []
     return render_template("home.html")
 
 
 @app.route('/satisfaction_survey', methods=["GET"])
 def load_survey():
     answer = request.args
-    counter = len(responses) - 1
+    counter = 0
     if answer:
-        responses.append(answer)
-        session["satisfaction"] = responses
+        session['satisfaction'].append(answer)
+        counter = len(session['satisfaction'])
+        # import pdb
+        # pdb.set_trace()
     survey = surveys.satisfaction_survey
     questions = survey.questions
     render_template("survey.html")
-    if len(responses) < len(questions):
+    if len(session['satisfaction']) < len(questions):
         return render_template("question.html", question=questions[counter])
     return render_template("thanks.html")
