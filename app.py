b48dd6e10d5d17389a6c14e1cac88346220e32c1
Nathan Newman <lordnate.moose@gmail.com>
2022-09-26 23:23:40 -0500

    completed assignment

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
 app = Flask(__name__)
 
+responses = []
+counter = -1
+
+
 @app.route('/')
 def load_home():
     return render_template("home.html")
 
-@app.route('/satisfaction_survey')
+
+@app.route('/satisfaction_survey', methods=["GET"])
 def load_survey():
+    answers = request.args
+    if answers:
+        global counter
+        counter += 1
+        responses.append(answers)
     survey = surveys.satisfaction_survey
     questions = survey.questions
-    import pdb
-    pdb.set_trace()
-    return render_template("survey.html", questions=questions)
+    render_template("survey.html")
+    if len(responses) < len(questions):
+        return render_template("question.html", question=questions[counter])
+    return render_template("thanks.html")
